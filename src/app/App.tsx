4045462927621098fc3c705749a64a3aa75f6820
sultantekru <tekru.1313@gmail.com>
2024-03-26 16:38:27 +0300

    React router implement edildi.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Home } from '../pages/Home';
+import { Location } from '../pages/Location';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-export const App: React.FC=() => {
+
+const App: React.FC = () => {
   return (
     <div className='app'>
-
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/location" element={<Location />} />
+          <Route path="*" element={<Navigate to={"/"} />} />
+        </Routes>
+      </BrowserRouter>
+    </div >
   )
 }
 
---
+++ src/pages/Location/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Location: React.FC = () => {
+    return (
+        <div>
+            Location
+        </div>
+    )
+}
+
+
+
